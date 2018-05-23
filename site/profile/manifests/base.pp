2c0e49d87b3aca472d3c75687f59bdfaba60daae
LorenzoDevOps <37579955+LorenzoDevOps@users.noreply.github.com>
2018-05-23 13:57:12 +0200

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -7,4 +7,9 @@ class profile::base {
     ensure  => present,
     content => "Ceci est un README\nMon adresse ip est ${ipaddress}\n",
   }
+  
+   host { 'client': 
+    ip => '10.134.39.246',
+    host_aliases => 'node2',
+  }
 }

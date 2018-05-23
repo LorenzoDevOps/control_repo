9f928e0e86514ada09319033640b78c00d03dfff
LorenzoDevOps <37579955+LorenzoDevOps@users.noreply.github.com>
2018-05-23 12:31:27 +0200

    Update motd.pp

--- site/profile/manifests/motd.pp
+++ site/profile/manifests/motd.pp
@@ -1,7 +1,7 @@
 class profile::motd {
   file { '/etc/motd':
     ensure  => file,
-    mode    => 644,
+    mode    => '644',
     content => "Welcome to ${::hostname} - ${::fqdn} - ${::operatingsystem} ${::operatingsystemrelease} This node is under the control of Puppet ${::puppetversion} Have a lot of fun...enjoy the fish";
   }
 } 

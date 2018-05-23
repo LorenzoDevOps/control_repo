e19f8ef710830d04ad2e99789dc63a92d3929644
LorenzoDevOps <37579955+LorenzoDevOps@users.noreply.github.com>
2018-05-23 14:38:24 +0200

    Update motd.pp

--- site/profile/manifests/motd.pp
+++ site/profile/manifests/motd.pp
@@ -2,6 +2,6 @@ class profile::motd {
   file { '/etc/motd':
     ensure  => file,
     mode    => '1204',
-    content => "Bienvenue sur ${::hostname} - ${::fqdn} - ${operatingsystem} ${::operatingsystemrelease}\nCe noeud est sous le contrôle de Puppet ${::puppetversion}\n";
+    content => "Bienvenue sur ${hostname} - ${fqdn} - ${operatingsystem} ${operatingsystemrelease}\nCe noeud est sous le contrôle de Puppet ${puppetversion}\n";
   }
 } 

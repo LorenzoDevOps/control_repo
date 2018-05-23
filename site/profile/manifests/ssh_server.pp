9a4711d9fc1e739c96e1829b5bf744ca6abc17c9
LorenzoDevOps <37579955+LorenzoDevOps@users.noreply.github.com>
2018-05-23 10:47:41 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -7,11 +7,11 @@ class profile::ssh_server {
     ensure  => 'running',
     enable  => 'true',
   }
+  
   file { "/home/root/.ssh/id_rsa.pub":
     ensure => present,
-    source => "puppet:///modules/user-sshkey/root/id_rsa.pub",
+    source => "/root/.ssh/id_rsa.pub",
     owner   => root,
-    group   => root,
-    mode    => 0644,
+    group   => root,    
   }
 }

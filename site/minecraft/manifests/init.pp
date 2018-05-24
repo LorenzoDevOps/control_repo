495b972e20a2fe32353675f585b2598b16e7a376
LorenzoDevOps <37579955+LorenzoDevOps@users.noreply.github.com>
2018-05-24 11:50:20 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -8,6 +8,14 @@ class minecraft {
     content =>  'eula=true',
   }
   
+  file { '/etc/systemd/system/minecraft.service':
+    ensure => file,
+    content => "[Unit]\nDescription=Minecraft Server\nWants=network.target\nAfter=network-online.target\nRequires=network-online.target\n\n[Service]\nWorkingDirectory=/opt/minecraft\nExecStart=/usr/bin/java -Xmx512M -Xms32M -jar server.jar nogui\nRestart=always\n\n[Install]\nWantedBy=multi-user.target\n",
+    owner  => 'root',
+    group  => 'root',
+    mode    => '0644',
+  }
+  
   package {'default-jre':
     ensure  =>  present,
   }

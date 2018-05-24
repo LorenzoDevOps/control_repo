a6f89963fed2d545aa62bb55729ab249864aaa64
LorenzoDevOps <37579955+LorenzoDevOps@users.noreply.github.com>
2018-05-24 12:59:06 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,3 +1,4 @@
+
 class minecraft {
   file {'/opt/minecraft':
     ensure  =>  directory,
@@ -7,17 +8,21 @@ class minecraft {
     ensure  =>  file,
     content =>  'eula=true',
   }
-    
+  
+  file {'/opt/minecraft/server.jar':
+    ensure  => file,
+    source  => 'puppet:///modules/minecraft/server.jar'
+  }
+  
   package {'default-jre':
     ensure  =>  present,
   }
   
-  file { '/etc/systemd/system/minecraft.service':
-    ensure => file,
-    content => "[Unit]\nDescription=Minecraft Server\nWants=network.target\nAfter=network-online.target\nRequires=network-online.target\n\n[Service]\nWorkingDirectory=/opt/minecraft\nExecStart=/usr/bin/java -Xmx512M -Xms32M -jar server.jar nogui\nRestart=always\n\n[Install]\nWantedBy=multi-user.target\n",
-    owner  => 'root',
-    group  => 'root',
-    mode    => '0644',
+  file {'/etc/systemd/system/minecraft.service':
+    ensure  =>  file,
+    source  =>  'puppet:///modules/minecraft/minecraft.service',
+    # content => "[Unit]\nDescription=Minecraft Server\nWants=network.target\nAfter=network-online.target\nRequires=network-online.target\n\n[Service]\nWorkingDirectory=/opt/minecraft\nExecStart=/usr/bin/java -Xmx512M -Xms32M -jar server.jar nogui\nRestart=always\n\n[Install]\nWantedBy=multi-user.target\n",
+
   }
   
   service {'minecraft':

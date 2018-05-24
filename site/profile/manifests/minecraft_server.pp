a6e4c3a6f5bae8ff3bb646691e7a2f84a4f77589
LorenzoDevOps <37579955+LorenzoDevOps@users.noreply.github.com>
2018-05-24 11:29:21 +0200

    Update minecraft_server.pp

--- site/profile/manifests/minecraft_server.pp
+++ site/profile/manifests/minecraft_server.pp
@@ -1,12 +1,17 @@
 
 class profile::minecraft_server {
-  user { 'admin':
-   ensure => present,
+  include ::wget
+  
+  file { '/opt/minecraft':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode    => '0644',
   }
-
-  file { '/root/README':
-   ensure  => present,
-   content => "[Unit]\n
+  
+  file { '/etc/systemd/system/minecraft.service':
+      ensure => file,
+      content => "[Unit]\n
 Description=Minecraft Server\n
 Wants=network.target\n
 After=network-online.target\n
@@ -18,5 +23,16 @@ ExecStart=/usr/bin/java -Xmx512M -Xms32M -jar server.jar nogui\n
 Restart=always\n
 \n
 [Install]\n
-WantedBy=multi-user.target",\n
+WantedBy=multi-user.target\n",
+      owner  => 'root',
+      group  => 'root',
+      mode    => '0644',
   }
+
+  wget::fetch { 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar':
+    destination => '/opt/minecraft/',
+    timeout     => 15,
+    verbose     => true,
+  }
+
+}

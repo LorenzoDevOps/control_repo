f6b0a3ac36c683561edb6384047cdc9477b7793b
LorenzoDevOps <37579955+LorenzoDevOps@users.noreply.github.com>
2018-05-24 11:25:41 +0200

    Update minecraft_server.pp

--- site/profile/manifests/minecraft_server.pp
+++ site/profile/manifests/minecraft_server.pp
@@ -1,1 +1,22 @@
 
+class profile::minecraft_server {
+  user { 'admin':
+   ensure => present,
+  }
+
+  file { '/root/README':
+   ensure  => present,
+   content => "[Unit]\n
+Description=Minecraft Server\n
+Wants=network.target\n
+After=network-online.target\n
+Requires=network-online.target\n
+\n
+[Service]\n
+WorkingDirectory=/opt/minecraft\n
+ExecStart=/usr/bin/java -Xmx512M -Xms32M -jar server.jar nogui\n
+Restart=always\n
+\n
+[Install]\n
+WantedBy=multi-user.target",\n
+  }
